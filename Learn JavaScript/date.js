9484edc0cf79b3bba3d5278230e7af6fb073b833
Pravin <singhpravin17a2@gmail.com>
2023-01-03 16:28:37 +0530

    Added more concept

---
+++ Learn JavaScript/date.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const currentdate = new Date();
+
+console.log(currentdate);
+console.log(currentdate.getFullYear());
+console.log(currentdate.getMonth());
+console.log(currentdate.getDay());
+console.log(currentdate.getDate());
+console.log(currentdate.getHours());
+console.log(currentdate.getMinutes());
+console.log(currentdate.getSeconds());
+console.log(currentdate.toDateString());
+console.log(currentdate.toString());
---
+++ Learn JavaScript/math.js
@@ -0,0 +1,14 @@
+"Use strict";
+
+// console.log(Math.PI);
+// console.log(Math.random());
+// console.log(Math.random() * 10);
+// console.log(Math.min(10, 12, 78, 45, 2, 06));
+// console.log(Math.max(10, 12, 78, 45, 2, 06));
+// console.log(Math.round(4.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.ceil(4.5));
+// console.log(Math.pow(4, 2));
+// console.log(Math.sin(90));
+// console.log(Math.cos(60));
+// console.log(Math.tan(45));
