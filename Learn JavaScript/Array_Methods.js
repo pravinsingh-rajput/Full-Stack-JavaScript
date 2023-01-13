603028050190cd84e72469e727d3c7ee24ce5a1e
pravinsingh-rajput <singhpravin17a2@gmail.com>
2023-01-13 19:04:33 +0530

    Working on Array Methods

---
+++ Learn JavaScript/Array_Methods.js
@@ -0,0 +1,18 @@
+let arr = [10, 15, 78, 918, 25, 45, 245, 754];
+
+// // at()
+// console.log(
+//   ` At Methods prints the value of the array at index of at(i) : ${arr.at(4)}`
+// );
+
+// // Concat()
+
+// let arr2 = ["Bread", "Butter", "Cheese"];
+// let arr3 = ["Pasta", "Pizza", "Burger"];
+
+// let concated1 = arr.concat(arr2);
+// let concated2 = arr.concat(arr2).concat(arr3);
+
+// console.log(
+//   ` Conact Methods Adds the number of array after concat() : \n ${concated1} \n ${concated2}}`
+// );
