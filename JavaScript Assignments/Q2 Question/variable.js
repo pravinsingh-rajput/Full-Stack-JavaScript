49a1bc58c1966d17428f7941c6b9e3564b73788b
pravinsingh-rajput <singhpravin17a2@gmail.com>
2023-01-21 09:15:42 +0530

    Question 2

---
+++ JavaScript Assignments/Q2 Question/variable.js
@@ -0,0 +1,7 @@
+let firstname = "Pravin";
+let lastname = "Singh";
+let marital_status = "Unmarried";
+let country = "India";
+let age = 21;
+
+console.log(`${firstname} ${lastname} ${marital_status} ${country} ${age}`);
