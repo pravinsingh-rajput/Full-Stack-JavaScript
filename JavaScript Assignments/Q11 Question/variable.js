28876f50ba4826987f43d1833cf5c8002a7dc828
pravinsingh-rajput <singhpravin17a2@gmail.com>
2023-01-21 11:35:57 +0530

    Working

---
+++ JavaScript Assignments/Q11 Question/variable.js
@@ -0,0 +1,14 @@
+let date = new Date();
+
+// console.log(date);
+// console.log(date.getFullYear());
+// console.log(date.getMonth() + 1);
+// console.log(date.getDate());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+
+const olddate = new Date(1970, 0, 2, 00, 00, 00, 00);
+
+console.log(olddate);
+
+console.log(olddate / 1000);
