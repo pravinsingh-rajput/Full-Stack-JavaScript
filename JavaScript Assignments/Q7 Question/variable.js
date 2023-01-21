226ea5843f133262d2f9dfea12da376fbaf8de6f
pravinsingh-rajput <singhpravin17a2@gmail.com>
2023-01-21 09:22:26 +0530

    Question 7

---
+++ JavaScript Assignments/Q7 Question/variable.js
@@ -0,0 +1,14 @@
+let arr = [
+  "HTML",
+  "CSS",
+  "Nodejs",
+  "Pw Skills",
+  "Tailwind",
+  "React",
+  "Mongodb",
+  "Pw Skills",
+  "Empty",
+];
+
+console.log(arr.indexOf("Pw Skills"));
+console.log(arr.lastIndexOf("Pw Skills"));
