e1644b0f8e02e463d519c7d3354535f397ffd6e2
pravinsingh-rajput <singhpravin17a2@gmail.com>
2023-01-21 09:32:11 +0530

    Question 9

---
+++ JavaScript Assignments/Q9 Question/variable.js
@@ -0,0 +1,10 @@
+// Truthy
+console.log("Truthy Values");
+console.log(Boolean("Pravin"));
+console.log(Boolean(41));
+console.log(Boolean(true), "\n");
+// Falsy
+console.log("Falsy Values");
+console.log(Boolean());
+console.log(Boolean(0));
+console.log(Boolean(null));
