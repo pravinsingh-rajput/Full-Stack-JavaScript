6a12af899c0d3b053bc5e375fd8ccf2ff65d2c3a
pravinsingh-rajput <singhpravin17a2@gmail.com>
2023-01-21 09:15:14 +0530

    Question 1

---
+++ JavaScript Assignments/Q1 Question/variable.js
@@ -0,0 +1,23 @@
+let string_value = "Pravin"; //String
+let Number_value = 2023; //Number
+let bool_value = true; //Boolean
+let undef_value = undefined; //Undefined
+let null_value = null; //NUll
+
+console.log(
+  "\n",
+  string_value,
+  typeof string_value,
+  "\n",
+  Number_value,
+  typeof Number_value,
+  "\n",
+  bool_value,
+  typeof bool_value,
+  "\n",
+  undef_value,
+  typeof undef_value,
+  "\n",
+  null_value,
+  typeof null_value
+);
