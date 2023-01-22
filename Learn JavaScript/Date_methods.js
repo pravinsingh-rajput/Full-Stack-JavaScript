fd518aa3d7f1053e487da3a4456733c94d9f044b
pravinsingh-rajput <singhpravin17a2@gmail.com>
2023-01-22 18:05:26 +0530

    Working

---
+++ Learn JavaScript/Date_methods.js
@@ -0,0 +1,13 @@
+"Use Strict ";
+
+// Syantax  new Date(year, monthIndex, day, hours, minutes, seconds, milliseconds)
+const date1 = new Date("2000-08-17T05:30:00");
+const date2 = new Date("August 17, 2000 05:30:00");
+
+// console.log(date1);
+// console.log(date2);
+
+const date3 = new Date("2000-12-17T05:30:00");
+// console.log(date3);
+
+const date = new Date();
