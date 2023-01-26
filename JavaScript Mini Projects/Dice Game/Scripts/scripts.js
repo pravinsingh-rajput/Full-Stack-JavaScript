7025aa34d034e1251e52dde5702f32e562d7ed52
pravinsingh-rajput <singhpravin17a2@gmail.com>
2023-01-26 18:53:53 +0530

    Added Winner

--- JavaScript Mini Projects/Dice Game/Scripts/scripts.js
+++ JavaScript Mini Projects/Dice Game/Scripts/scripts.js
@@ -11,7 +11,7 @@ const animatedice = document.querySelector(".animatedice");
 const rolldice = document.querySelector(".rolldice");
 const player1 = document.querySelector(".player1");
 const player2 = document.querySelector(".player2");
-const winningplayer = document.querySelector(".winningplayer");
+const winningplayer = document.querySelector(".winning");
 let score1 = Number(document.querySelector(".totalscore_0").innerText);
 let score2 = Number(document.querySelector(".totalscore_1").innerText);
 
@@ -20,6 +20,7 @@ let live2 = document.querySelector(".live2");
 
 let tempScore = 0;
 let totalscore = 0;
+let playing = true;
 
 function showanimation(numberdice) {
   dice.classList.add("hidden");
@@ -74,61 +75,85 @@ roll.addEventListener("click", function () {
   let numberdice = Math.trunc(Math.random() * 6 + 1);
   showanimation(numberdice);
 
-  if (numberdice !== 1) {
-    setTimeout(() => {
-      tempScore = `${Number(tempScore) + Number(numberdice)}`;
-      document.querySelector(`.currscore_${activeplayer}`).innerText =
-        tempScore;
-    }, 2000);
-    winning();
+  let winner =
+    Number(document.querySelector(`.totalscore_${activeplayer}`).innerText) +
+    Number(tempScore) +
+    Number(numberdice);
+  if (playing && winner < 10) {
+    if (numberdice !== 1) {
+      setTimeout(() => {
+        tempScore = `${Number(tempScore) + Number(numberdice)}`;
+        document.querySelector(`.currscore_${activeplayer}`).innerText =
+          tempScore;
+      }, 2000);
+      winning();
+    } else {
+      setTimeout(() => {
+        tempScore = 0;
+        document.querySelector(`.currscore_${activeplayer}`).innerText =
+          tempScore;
+        change_effect(activeplayer);
+        activeplayer = activeplayer == 0 ? 1 : 0;
+        active(activeplayer);
+      }, 2000);
+    }
   } else {
     setTimeout(() => {
-      tempScore = 0;
-      document.querySelector(`.currscore_${activeplayer}`).innerText =
-        tempScore;
-      change_effect(activeplayer);
-      activeplayer = activeplayer == 0 ? 1 : 0;
-      active(activeplayer);
+      document.querySelector(`.totalscore_${activeplayer}`).innerText = winner;
+      winningplayer.classList.add("flex");
+      winningplayer.classList.remove("hidden");
+      playing = false;
     }, 2000);
   }
 });
 
 hold.addEventListener("click", function () {
-  totalscore =
-    Number(document.querySelector(`.totalscore_${activeplayer}`).innerText) +
-    Number(tempScore);
+  if (playing) {
+    totalscore =
+      Number(document.querySelector(`.totalscore_${activeplayer}`).innerText) +
+      Number(tempScore);
 
-  document.querySelector(`.totalscore_${activeplayer}`).innerText = totalscore;
-  totalscore = 0;
-  tempScore = 0;
-  document.querySelector(`.currscore_${activeplayer}`).innerText = tempScore;
-  change_effect(activeplayer);
-  activeplayer = activeplayer === 1 ? 0 : 1;
-  active(activeplayer);
+    document.querySelector(`.totalscore_${activeplayer}`).innerText =
+      totalscore;
+    totalscore = 0;
+    tempScore = 0;
+    document.querySelector(`.currscore_${activeplayer}`).innerText = tempScore;
+    change_effect(activeplayer);
+    activeplayer = activeplayer === 1 ? 0 : 1;
+    active(activeplayer);
+  }
 });
 
 // Reset
 reset.addEventListener("click", function () {
-  console.log("clicked");
-  dice.classList.add("hidden");
-  rolldice.classList.add("hidden");
-  animatedice.classList.add("hidden");
-  roll.classList.remove("hidden");
-  totalscore = 0;
-  tempScore = 0;
-  document.querySelector(`.currscore_${activeplayer}`).textContent = tempScore;
-  document.querySelector(`.totalscore_${activeplayer}`).textContent =
-    totalscore;
+  if (!playing) {
+    console.log("clicked");
+    dice.classList.add("hidden");
+    rolldice.classList.add("hidden");
+    animatedice.classList.add("hidden");
+    roll.classList.remove("hidden");
+    totalscore = 0;
+    tempScore = 0;
+    document.querySelector(`.currscore_${activeplayer}`).textContent =
+      tempScore;
+    document.querySelector(`.totalscore_${activeplayer}`).textContent =
+      totalscore;
 
-  if (!activeplayer == 0) {
-    change_effect(activeplayer);
+    if (!activeplayer == 0) {
+      change_effect(activeplayer);
+    }
+    activeplayer = activeplayer === 0 ? 1 : 0;
+
+    document.querySelector(`.totalscore_${activeplayer}`).textContent =
+      totalscore;
+    document.querySelector(`.currscore_${activeplayer}`).textContent =
+      tempScore;
+
+    activeplayer = 0;
+    active(activeplayer);
+    playing = true;
+
+    winningplayer.classList.add("hidden");
+    winningplayer.classList.remove("flex");
   }
-  activeplayer = activeplayer === 0 ? 1 : 0;
-
-  document.querySelector(`.totalscore_${activeplayer}`).textContent =
-    totalscore;
-  document.querySelector(`.currscore_${activeplayer}`).textContent = tempScore;
-
-  activeplayer = 0;
-  active(activeplayer);
 });
