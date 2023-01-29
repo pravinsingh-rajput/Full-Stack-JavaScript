46eba6ca6f189a5af7d9e7a05a5199d20d13c574
pravinsingh-rajput <singhpravin17a2@gmail.com>
2023-01-29 18:30:58 +0530

    Destructuring

---
+++ Learn JavaScript/Destructuring.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const arr = [11, 12, 13, 14];
+
+// let a = arr[0];
+// let b = arr[1];
+// let c = arr[2];
+// let d = arr[3];
+
+// console.log(a, b, c, d);
+
+const [p, q, r, s] = arr;
+console.log(p, q, r, s);
+
+const [l, m, n] = arr;
+console.log(l, m, n);
+
+const [x, , y, z] = arr;
+console.log(x, y, z);
