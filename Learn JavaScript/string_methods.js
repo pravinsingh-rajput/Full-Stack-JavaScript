7e5e2d3823d0051c10d6dcb289ebd39ff8e6dccf
pravinsingh-rajput <singhpravin17a2@gmail.com>
2023-01-31 12:18:44 +0530

    Working on String Methos

---
+++ Learn JavaScript/string_methods.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const mynamne = "Pravin Singh";
+
+// console.log(mynamne.toUpperCase());
+// console.log(mynamne.toLocaleLowerCase());
+
+// const tempname = "   PRaVin  ";
+
+// console.log(tempname.toLocaleLowerCase().trim());
+
+const random = "  PraVIn ";
+
+let correct1 = random.toLocaleLowerCase().trim();
+console.log(correct1);
+
+let correct2 = correct1[0].toUpperCase() + correct1.slice(1);
+console.log(correct2);
+
+// replace
+
+const dollar = "147,90$";
+
+const rupee = dollar.replace("$", " Rupees").replace(",", ".");
+console.log(rupee);
+
+let goodgreet = "Good Morning, Good Afternoon, Good Night";
+
+let badgreet = goodgreet.replaceAll("Good", "Bad");
+console.log(badgreet);
