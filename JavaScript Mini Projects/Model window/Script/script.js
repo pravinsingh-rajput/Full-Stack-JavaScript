6c4d31b4ccb30376c43d59d7c68321ce5f40e801
pravinsingh-rajput <singhpravin17a2@gmail.com>
2023-02-13 16:54:26 +0530

    Added popup window

--- JavaScript Mini Projects/Model window/Script/script.js
+++ JavaScript Mini Projects/Model window/Script/script.js
@@ -1,3 +1,21 @@
 "use strict";
 
 console.log("Connected");
+
+const btn = document.querySelectorAll(".button");
+const popup = document.querySelector(".popup");
+const close = document.querySelector(".close");
+
+btn[0].addEventListener("click", function () {
+  popup.classList.toggle("hidden");
+});
+btn[1].addEventListener("click", function () {
+  popup.classList.toggle("hidden");
+});
+btn[2].addEventListener("click", function () {
+  popup.classList.toggle("hidden");
+});
+
+close.addEventListener("click", function () {
+  popup.classList.toggle("hidden");
+});
