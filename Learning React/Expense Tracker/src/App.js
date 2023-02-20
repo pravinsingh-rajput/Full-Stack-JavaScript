7a9ea28286a461c3a78915c0b4c4fe8c24116d8f
pravinsingh-rajput <singhpravin17a2@gmail.com>
2023-02-20 19:35:52 +0530

    Working

--- Learning React/Expense Tracker/src/App.js
+++ Learning React/Expense Tracker/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import ExpenseItems from "./Components/ExpenseItems";
 
 function App() {
+  // Data
+
+  const data = [
+    {
+      id: "e1",
+      date: new Date(2023, 1, 20),
+      title: "Electric Bill",
+      amount: 500,
+    },
+    {
+      id: "e2",
+      date: new Date(2023, 0, 28),
+      title: "Desktop",
+      amount: 1230,
+    },
+    {
+      id: "e3",
+      date: new Date(2023, 0, 15),
+      title: "Books",
+      amount: 208,
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <ExpenseItems
+        date={data[0].date}
+        title={data[0].title}
+        amount={data[0].amount}
+      />
+      <ExpenseItems
+        date={data[1].date}
+        title={data[1].title}
+        amount={data[1].amount}
+      />
+      <ExpenseItems
+        date={data[2].date}
+        title={data[2].title}
+        amount={data[2].amount}
+      />
     </div>
   );
 }
---
+++ Learning React/Expense Tracker/src/Components/ExpenseItems.js
@@ -0,0 +1,23 @@
+import "./ExpenseItems.css";
+
+function ExpenseItems(props) {
+  const day = props.date.toLocaleString(navigator.language, { day: "2-digit" });
+  const month = props.date.toLocaleString(navigator.language, {
+    month: "long",
+  });
+  const year = props.date.getFullYear();
+
+  return (
+    <div className="expense_items">
+      <div className="expense_items_date">
+        <div className="expense_month">{month}</div>
+        <div className="expense_year">{year}</div>
+        <div className="expense_date">{day}</div>
+      </div>
+      <div className="expense_items_title"> {props.title} </div>
+      <div className="expense_items_amount"> $ {props.amount}</div>
+    </div>
+  );
+}
+
+export default ExpenseItems;
