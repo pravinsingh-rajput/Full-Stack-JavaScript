914759e34374a5eac5d7ae8dc7e4a86d04f9361c
pravinsingh-rajput <singhpravin17a2@gmail.com>
2023-03-15 10:47:49 +0530

    Working On F&Q

--- React Webiste Project/netflix/src/App.js
+++ React Webiste Project/netflix/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import HeroSection from "./Component/HeroSection/HeroSection";
 import FeaturesSection from "./Component/FeaturesSection/FeaturesSection";
+import FrequentQuetions from "./Component/Questions/F&Q";
 
 function App() {
   return (
     <div className="App">
       <HeroSection className="hero_section" />
-      <FeaturesSection className="Feature_section" />
+      <FeaturesSection className="feature_section" />
+      <FrequentQuetions className="frequent_quetions" />
     </div>
   );
 }
