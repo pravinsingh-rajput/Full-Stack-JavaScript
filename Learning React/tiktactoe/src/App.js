97543b34cc28af1f99b57f07f1835070fff273f6
pravinsingh-rajput <singhpravin17a2@gmail.com>
2023-03-16 12:14:43 +0530

    Working on TikTakToe

---
+++ Learning React/tiktactoe/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Icon from "./Components/Icon";
+
+function App() {
+  return (
+    <div className="App">
+      <Icon />
+    </div>
+  );
+}
+
+export default App;
---
+++ Learning React/tiktactoe/src/Components/Icon.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { FaTimes, FaRegCircle, FaPen } from "react-icons/fa";
+
+function Icon() {
+  return (
+    <div>
+      <FaTimes className="icon" />
+      <FaRegCircle className="icon" />
+      <FaPen className="icon" />
+    </div>
+  );
+}
+
+export default Icon;
